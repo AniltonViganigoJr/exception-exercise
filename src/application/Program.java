4e32ba3ba5fe6a1102b8ac32e931ec4f082a0daf
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-09-23 22:45:04 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,45 +9,38 @@ import model.entities.Reservation;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException{
-		
+	public static void main(String[] args) throws ParseException {
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		System.out.print("Room number:");
 		int roomNumber = sc.nextInt();
 		sc.nextLine();
 		System.out.print("Check-in (DD/MM/YYYY):");
-		Date checkin = sdf.parse(sc.next()); 
+		Date checkin = sdf.parse(sc.next());
 		System.out.print("Check-out (DD/MM/YYYY):");
-		Date checkout = sdf.parse(sc.next()); 
-		
-		if(!checkout.after(checkin)) {
+		Date checkout = sdf.parse(sc.next());
+
+		if (!checkout.after(checkin)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date!");
-		}else {
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
 			System.out.println(reservation.toString());
 			System.out.println("\nEnter data to update the reservation:");
 			System.out.print("Check-in (DD/MM/YYYY):");
-			checkin = sdf.parse(sc.next()); 
+			checkin = sdf.parse(sc.next());
 			System.out.print("Check-out (DD/MM/YYYY):");
 			checkout = sdf.parse(sc.next());
-		
-			Date now = new Date();
-			if (checkin.before(now) || checkout.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates!");
-			}
-			else if(!checkout.after(checkin)){
-				System.out.println("Error in reservation: Check-out date must be after check-in date!");
-			}
-			else {
-				reservation.updateDates(checkin, checkout);
+
+			String error = reservation.updateDates(checkin, checkout);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			} else {
 				System.out.println(reservation.toString());
 			}
 		}
-		
 		sc.close();
 	}
 }
